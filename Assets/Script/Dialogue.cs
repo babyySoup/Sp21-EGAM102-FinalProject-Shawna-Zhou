3e25699a6999204f2a999020695ebad4ae4d587c
babyySoup <71286763+babyySoup@users.noreply.github.com>
2021-04-08 18:08:08 -0700

    uo

---
+++ Assets/Script/Dialogue.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class Dialogue 
+{
+    // pass into the dialogue manager.
+
+    [TextArea(3,15)]
+    public string[] sentences;
+    public string name;
+    
+}
---
+++ Assets/Script/DialogueManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    //UI stuff
+    public TMPro.TMP_Text nameText;
+    public TMPro.TMP_Text dialogueBoxText;
+    
+    
+    //keep track of sentences 
+    private Queue<string> sentences; 
+     
+    
+    
+    void Start()
+    {
+        sentences = new Queue<string>();
+
+    }
+
+    public void StartConversation (Dialogue dialogue)
+    {
+        nameText.text = dialogue.name;
+
+        //clear earlier sentences
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        ShowNextSentence();
+    } 
+
+    public void ShowNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndConversation();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        dialogueBoxText.text = sentence; 
+    }
+
+    void EndConversation()
+    {
+        Debug.Log("Nothing else to talk about...");
+    }
+
+}
